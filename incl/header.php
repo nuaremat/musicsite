dd200d2f1806fbdfe6703bdfec4bb55db893221a
Ollis <nyren.olof@gmail.com>
2017-01-09 13:29:29 +0100

    Snyggade till lite här bara

--- incl/header.php
+++ incl/header.php
@@ -19,16 +19,16 @@
     <meta charset="utf-8" />
     <link href="style/stilmall.css" rel="stylesheet" type="text/css" />
 
-    <?php if(isset($accordion)) { ?>
+    <?php if(isset($accordion)) : ?>
         <!-- 20150914 1.11.1 -> 1.11.4 
             20161019 1.11.4 -> 1.12.1 -->
         <link rel="stylesheet" href="jquery-ui-1.12.1/jquery-ui.css" />
-    <?php } ?>
+    <?php endif; ?>
 
-    <?php if(isset($slimbox)) { ?>
+    <?php if(isset($slimbox)) : ?>
         <!-- 20150914 2 -> 2.05 -->
         <link rel="stylesheet" href="slimbox-2.05/css/slimbox2.css" />
-    <?php } ?>
+    <?php endif; ?>
 
     <title><?php echo($title); ?></title>
 
